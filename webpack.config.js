25eee940b0d77bddc07a016e61828612c9fbe456
bagdasarjan <o.bagdasarjan@cft.ru>
2021-11-15 16:12:22 +0700

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  target: 'node',
+  entry: './server.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.bundle.js',
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        path.resolve(__dirname, "google-client", "credentials.json"),
+      ],
+    }),
+  ]
+};
