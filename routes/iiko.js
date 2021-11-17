1a9dd2d257ad5a42ee3d410d7b8fb67946c7ed7a
Ovik <ovik-94@yandex.ru>
2021-11-17 15:22:11 +0700

    edit

--- routes/iiko.js
+++ routes/iiko.js
@@ -1,20 +1,22 @@
 const router = require("express").Router();
 const iikoWebApi = require("../iiko-web/api");
-const tbot = require('../telegram-bot/tbot');
-const getTelegramChatId = require('../telegram-bot/get-telegram-chat-id');
-const allowedAmounts = require('../routes/iiko-data/allowed-amounts-bar');
+const tbot = require("../telegram-bot/tbot");
+const getTelegramChatId = require("../telegram-bot/get-telegram-chat-id");
+const allowedAmounts = require("../routes/iiko-data/allowed-amounts-bar");
+const easyTable = require("easy-table");
 
 router.get("/bar-balance", async function (req, res, next) {
   const productsBalance = await iikoWebApi.getBarBalance();
 
-  const filteredProduct = productsBalance.filter(product => product.product.categoryName === 'Напитки' ||
-    product.product.categoryName === 'Крепкий Алкоголь');
+  const filteredProduct = productsBalance.filter(
+    (product) => product.product.categoryName === "Напитки" || product.product.categoryName === "Крепкий Алкоголь"
+  );
 
-  const transformData = filteredProduct.map(item => ({
+  const transformData = filteredProduct.map((item) => ({
     name: item.product.name,
     category: item.product.categoryName,
     unit: item.product.mainUnitName,
-    balance: item.amount
+    balance: item.amount,
   }));
 
   const data = [];
@@ -30,15 +32,27 @@ router.get("/bar-balance", async function (req, res, next) {
     }
   }
 
+  const table = new easyTable();
 
   if (data.length > 0) {
-    let message = '';
     data.forEach((item, index) => {
-      message += `${index+ 1}) ${item.name} осталось ${item.balance} ${item.unit} \n`;
-    })
-
-    await tbot.sendMessage(getTelegramChatId('balance'), message)
+      table.cell("Позиция", index + 1, easyTable.string());
+      table.cell("Название", item.name, easyTable.string());
+      table.cell("Остаток", `${item.balance} ${item.unit}`, easyTable.string());
+      table.newRow();
+    });
   }
+
+  await tbot.sendMessage(getTelegramChatId("balance"), table.toString());
+
+  // if (data.length > 0) {
+  //   let message = "";
+  //   data.forEach((item, index) => {
+  //     message += `${index + 1}) ${item.name} осталось ${item.balance} ${item.unit} \n`;
+  //   });
+
+  //   await tbot.sendMessage(getTelegramChatId("balance"), message);
+  // }
 
   return res.json({ status: "OK", data });
 });
