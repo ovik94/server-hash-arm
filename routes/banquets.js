d3f0dc1276274f9da015a00a98e3a6a6339dfbd5
bagdasarjan <o.bagdasarjan@cft.ru>
2021-12-27 12:41:02 +0700

    перенес логику минимальных остатков в google table

--- routes/banquets.js
+++ routes/banquets.js
@@ -1,16 +1,8 @@
 const { format } = require('date-fns');
 const router = require("express").Router();
-const fs = require('fs');
-const path = require('path');
-const pdf = require('pdf-creator-node');
+const easyTable = require("easy-table");
 const getTelegramChatId = require('../telegram-bot/get-telegram-chat-id');
 const tbot = require('../telegram-bot/tbot');
-
-const options = {
-  format: "A4",
-  orientation: "portrait",
-  border: "10mm"
-};
 
 const formatterDate = (date, skeleton) => format(date, skeleton);
 
@@ -18,8 +10,6 @@ router.post("/save", async function (req, res, next) {
   const { body } = req;
 
   let status = 'OK';
-
-  const html = fs.readFileSync(path.resolve(__dirname, './templates/banquet.html'), 'utf8');
 
   const menu = [];
 
@@ -46,25 +36,33 @@ router.post("/save", async function (req, res, next) {
     data.serviceFee = body.serviceFee;
   }
 
-  const filename = `${body.name}-${formatterDate(new Date(body.date), 'dd.MM.yyyy')}.pdf`;
-  const document = {
-    html: html,
-    data,
-    path: `./${filename}`,
-    type: "buffer",
-  };
+  const table = new easyTable();
 
-  pdf
-    .create(document, options)
-    .then((res) => {
-      tbot.sendDocument(getTelegramChatId('banquets'), res, {}, { filename }).then(() => {
-        status = 'ERROR_BOT_SEND_MESSAGE';
-      });
-    })
-    .catch((error) => {
-      status = 'ERROR_CREATE_PDF';
+  if (data.menu.length > 0) {
+    data.menu.forEach((item, index) => {
+      table.cell("Название", item.title, easyTable.string());
+      table.cell("Цена", item.price, easyTable.string());
+      table.cell("Кол-во", item.count, easyTable.string());
+      table.cell("Вес", item.weight, easyTable.string());
+      table.newRow();
     });
+  }
 
+  const message = `<b>Резерв банкета. Принял адмимнистратор ${data.admin}</b>
+<i>Имя: ${data.name}</i>
+<i>Номер телефона: ${data.phone}</i>
+<i>Количество гостей: ${data.personsCount}</i>
+<i>Дата: ${data.date}</i>
+<pre>${table.toString()}</pre>
+`;
+
+  await tbot.sendMessage(
+    getTelegramChatId("banquets"),
+    message,
+    { parse_mode: 'HTML' }
+  ).catch(() => {
+    status = 'ERROR_BOT_SEND_MESSAGE';
+  });
 
   return res.json({ status });
 });
