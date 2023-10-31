123f9752d1c22eef8f0e1e0ea66b1e63aa7d7a18
bagdasarjan <o.bagdasarjan@cft.ru>
2023-10-31 22:02:25 +0700

    перевод expenses в mongo

--- model/tempExpenses.js
+++ model/tempExpenses.js
@@ -5,11 +5,11 @@ const tempExpensesSchema = new Schema({
   sum: { type: String, required: true },
   comment: String,
   counterparty: String,
-  category: [{
+  category: {
     title: { type: String, required: true },
     icon: { type: String, required: true },
     counterpartyType: String
-  }]
+  }
 }, {
   versionKey: false,
   toJSON: {
