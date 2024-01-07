830e3e27e7c1a16755cf1c8ec89949a35fe27819
Your Name <you@example.com>
2024-01-07 16:24:24 +0700

    добавил новый ланч

--- controllers/lunch.js
+++ controllers/lunch.js
@@ -3,44 +3,47 @@ const tbot = require("../src/telegram-bot/tbot");
 const getTelegramChatId = require("../src/telegram-bot/get-telegram-chat-id");
 const axios = require("axios");
 
-async function sendLunchTelegram (req, res) {
+async function sendLunchTelegram(req, res) {
   const images = {
     1: [
-      'AgACAgIAAxkBAAIBamIfN_6PmOKI51fI0cbKR3w2i-lTAALKtzEbblr4SA1nwCS7hlcxAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBa2IfOAn91PC3qFU_TO9PpV03ed74AALLtzEbblr4SNkYWHMJTD94AQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBbGIfOBQwfuvBo3MiqXJSX4TEwguvAALMtzEbblr4SOiTB_1n7bHCAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBbWIfOBxshXbRrECb6chbWV41EhbYAALNtzEbblr4SMW1PETv9TjiAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBbmIfOCU1YmvEtJ8anmYqtdOqKbBTAALPtzEbblr4SK-jA6TOJXQNAQADAgADeQADIwQ'
+      "AgACAgIAAxkBAAIEkGWabJG_a32JLsvZaR736DgpSIcgAAJn0zEbGr3YSCa570T22HnxAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEkWWabLIMVOMle878XQ7d1BOT0MxHAAJp0zEbGr3YSHamOK1A3jOCAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEkmWabMPmb7a6uGFOSz2yLaCh5C3gAAJq0zEbGr3YSBnoMVONVzLJAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEk2WabNh2-PyCDNZpxzNjqNgPTpamAAJr0zEbGr3YSEz9VTPbBswdAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIElGWabOjuZl5fNVWJ91RueR7Ts1IPAAJt0zEbGr3YSHD1-dE_FycRAQADAgADeQADNAQ",
     ],
     2: [
-      'AgACAgIAAxkBAAIBW2IfNNu966PCzluhe5Tnz4kyVgi3AAKwtzEbblr4SMA3Z5u7OohiAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBXGIfNx7sMRNWS3oWS2DgyPlUTrIPAAJvuTEb-5b4SAAB_a0S7NaxGwEAAwIAA3kAAyME',
-      'AgACAgIAAxkBAAIBXWIfNzeASHrorwsmphxYnlRO6-LLAAKytzEbblr4SAz8kPicXHrwAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBXmIfN0vcwlo6riWSzEPX1qC9hqKMAAKztzEbblr4SG-rmpbx5xcVAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBX2IfN16mgTzto8fNdyZCjzLArI-4AAK1tzEbblr4SO2zRgQMvU7-AQADAgADeQADIwQ'
+      "AgACAgIAAxkBAAIEgWWaauwQPvGfZVAq8fJc3HyxNk1VAAJP0zEbGr3YSAbOL5_eCGpHAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEgmWaayKD75ZoMF4qBGOyZ23HFU1vAAJT0zEbGr3YSMhYAAFBuPvY6AEAAwIAA3kAAzQE",
+      "AgACAgIAAxkBAAIEg2WaazT5HaVk4njJXLQa9YfTB3NdAAJU0zEbGr3YSCDNKqj3DVyNAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEhGWaa0_YFVcUXCQmFcKm0eNIvPHFAAJZ0zEbGr3YSAQkYZTMfpbzAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEhWWaa5v5o4B-JLcgwkG3nNMkUI1UAAJa0zEbGr3YSLjTz5m6lP-AAQADAgADeQADNAQ",
     ],
     3: [
-      'AgACAgIAAxkBAAIBYGIfN4N4vQa4NTYmMCKequsaTEQGAAK2tzEbblr4SMNWgunFjhl9AQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBYWIfN47htKjtxsAEbJguo3IkaSSoAAK3tzEbblr4SP2yRrHhw72SAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBYmIfN5c4bufQJT7dQOe3eR4XG5CPAAK4tzEbblr4SLBTOqQqZ7aUAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBY2IfN6jsoW3Fj6GninpzbPf5thrBAALDtzEbblr4SDPOKH22YVluAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBZGIfN7F3swn3MC_7bPna5uEQicLrAALEtzEbblr4SDIRsGkOGkxnAQADAgADeQADIwQ'
+      "AgACAgIAAxkBAAIEhmWaa7XKama0EAJEYfbPHRq4n-0vAAJb0zEbGr3YSOoisFcHsFcuAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEh2Waa83DbFeUaL4Hkxx3offZWf7_AAJd0zEbGr3YSIvwPvGq3vfiAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEiGWaa-oNjtdlwup3gK5XzW6foXGSAAJe0zEbGr3YSAUqtNNR6fTtAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEiWWabBL37nJgjtujOMSRqz1IrPK-AAJf0zEbGr3YSMHgVQ0D4PzvAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEimWabCVdhWMgPDKlY-DAtI5O-7CoAAJg0zEbGr3YSDXVYdsxzwoaAQADAgADeQADNAQ",
     ],
     0: [
-      'AgACAgIAAxkBAAIBZWIfN8RhnQNL7p8uDgFbxSgCkRinAALFtzEbblr4SKPe-reZNPCLAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBZmIfN9OCKiCX84JrektwdBPb3NtFAALGtzEbblr4SNRcmC1iA5EdAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBZ2IfN931_KDOuUR0a07sm6muGqKEAALHtzEbblr4SL5MaGeaKa-ZAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBaGIfN-aU_rJgKhlQHBZnTG--REWWAALItzEbblr4SIQ85VZoPYibAQADAgADeQADIwQ',
-      'AgACAgIAAxkBAAIBaWIfN_J6bs4QtVt320FYtmuDvXbdAALJtzEbblr4SLzf9G2rL4_TAQADAgADeQADIwQ'
-    ]
+      "AgACAgIAAxkBAAIEi2WabDuhFZaaaTstO9LBeSrMPPrrAAJh0zEbGr3YSG7Kze1zAsYGAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEjGWabEy4qt1njEmpoDarS8v7CHeGAAJi0zEbGr3YSJXTrvzNnSN9AQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEjWWabF_u44gpRWsiaj9FqbR2RJZoAAJj0zEbGr3YSPAmWc1Pb4K2AQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEjmWabHLoelG_x7XNc8Sdse7VGSEtAAJk0zEbGr3YSL74D58GXguEAQADAgADeQADNAQ",
+      "AgACAgIAAxkBAAIEj2WabIJornQ2WJu7MKhmP6Qd_W_wAAJl0zEbGr3YSDoso0hUoOb4AQADAgADeQADNAQ",
+    ],
   };
 
-  const week = moment().week();
-  const day = moment().day();
+  const week = moment().isoWeek();
+  const day = moment().isoWeekday();
   const holiday = day === 6 || day === 7;
 
   if (!holiday) {
-    await tbot.sendPhoto(getTelegramChatId("channel"), images[week % 4][day - 1]);
+    await tbot.sendPhoto(
+      getTelegramChatId("channel"),
+      images[week % 4][day - 1]
+    );
   }
 
   return res.json({ status: "OK" });
@@ -49,55 +52,60 @@ async function sendLunchTelegram (req, res) {
 async function sendLunchVk(req, res, next) {
   const images = {
     1: [
-      'photo-211214337_457241017',
-      'photo-211214337_457241016',
-      'photo-211214337_457241019',
-      'photo-211214337_457241020',
-      'photo-211214337_457241018'
+      "photo-211214337_457243149",
+      "photo-211214337_457243148",
+      "photo-211214337_457243151",
+      "photo-211214337_457243152",
+      "photo-211214337_457243150",
     ],
     2: [
-      'photo-211214337_457241002',
-      'photo-211214337_457241001',
-      'photo-211214337_457241004',
-      'photo-211214337_457241005',
-      'photo-211214337_457241003'
+      // 1 неделя
+      "photo-211214337_457243134",
+      "photo-211214337_457243133",
+      "photo-211214337_457243136",
+      "photo-211214337_457243137",
+      "photo-211214337_457243135",
     ],
     3: [
-      'photo-211214337_457241007',
-      'photo-211214337_457241006',
-      'photo-211214337_457241009',
-      'photo-211214337_457241010',
-      'photo-211214337_457241008'
+      "photo-211214337_457243139",
+      "photo-211214337_457243138",
+      "photo-211214337_457243141",
+      "photo-211214337_457243142",
+      "photo-211214337_457243140",
     ],
     0: [
-      'photo-211214337_457241012',
-      'photo-211214337_457241011',
-      'photo-211214337_457241014',
-      'photo-211214337_457241015',
-      'photo-211214337_457241013'
-    ]
-  }
+      "photo-211214337_457243144",
+      "photo-211214337_457243143",
+      "photo-211214337_457243146",
+      "photo-211214337_457243147",
+      "photo-211214337_457243145",
+    ],
+  };
 
-  const week = moment().week();
-  const day = moment().day();
+  const week = moment().isoWeek();
+  const day = moment().isoWeekday();
   const holiday = day === 6 || day === 7;
-  let status = 'OK';
+  let status = "OK";
 
   if (!holiday) {
-    axios.post('https://broadcast.vkforms.ru/api/v2/broadcast?token=api_87768_YKQjQvoekX1ri4HGHKYRG4Wi', {
-      message: {
-        message: 'Меню бизнес-ланча на сегодня',
-        attachment: images[week % 4][day - 1]
-      },
-      list_ids: '1179243',
-      run_now: 1,
-    })
+    axios
+      .post(
+        "https://broadcast.vkforms.ru/api/v2/broadcast?token=api_87768_YKQjQvoekX1ri4HGHKYRG4Wi",
+        {
+          message: {
+            message: "Меню бизнес-ланча на сегодня",
+            attachment: images[week % 4][day - 1],
+          },
+          list_ids: "1179243",
+          run_now: 1,
+        }
+      )
       .then(function (response) {
-        console.log('Рассылка отправлена');
+        console.log("Рассылка отправлена");
       })
       .catch(function (error) {
-        console.log(error, 'Ошибка при отправке рассылки');
-        status = 'ERROR';
+        console.log(error, "Ошибка при отправке рассылки");
+        status = "ERROR";
       });
   }
 
