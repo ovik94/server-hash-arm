dd1a142742e32e8d28f86bce1c3c23b44d975b12
Ovik Bagdasaryan <you@example.com>
2024-07-03 22:59:38 +0700

    рефакторинг

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,41 +1,44 @@
-const gApi = require("../src/google-client/google-api");
+const { expensesController } = require("../src/google-client/controllers");
 
-async function getExpenses (req, res) {
+async function getExpenses(req, res) {
   try {
-    const reports = await gApi.getExpenses();
-    const result = reports.map(item => ({ ...item, category: item.category ? JSON.parse(item.category) : {} }));
+    const reports = await expensesController.getExpenses();
+    const result = reports.map((item) => ({
+      ...item,
+      category: item.category ? JSON.parse(item.category) : {},
+    }));
     return res.json({ status: "OK", data: result });
   } catch (err) {
-    return res.json({ status: 'ERROR', message: err.message });
+    return res.json({ status: "ERROR", message: err.message });
   }
 }
 
-async function addExpense (req, res) {
+async function addExpense(req, res) {
   const { body } = req;
 
   try {
-    await gApi.addExpense(body);
+    await expensesController.addExpense(body);
   } catch (err) {
-    return res.json({ status: 'ERROR', message: err.message });
+    return res.json({ status: "ERROR", message: err.message });
   }
 
-  return res.json({ status: 'OK' });
+  return res.json({ status: "OK" });
 }
 
-async function deleteExpense (req, res) {
+async function deleteExpense(req, res) {
   const { body } = req;
 
   if (!body.id) {
-    return res.json({ status: 'ERROR', message: 'Не указан id расхода' });
+    return res.json({ status: "ERROR", message: "Не указан id расхода" });
   }
 
   try {
-    await gApi.deleteExpense(body.id);
+    await expensesController.deleteExpense(body.id);
   } catch (err) {
-    return res.json({ status: 'ERROR', message: err.message });
+    return res.json({ status: "ERROR", message: err.message });
   }
 
-  return res.json({ status: 'OK' });
+  return res.json({ status: "OK" });
 }
 
 module.exports = { getExpenses, addExpense, deleteExpense };
