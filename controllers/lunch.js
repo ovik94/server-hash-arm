801c2c9a4bd9d12e98309bff90aee1de42089a33
Ovik Bagdasaryan <you@example.com>
2024-11-23 18:35:52 +0700

    добавил запрос определения недели ланча

--- controllers/lunch.js
+++ controllers/lunch.js
@@ -3,9 +3,20 @@ const tbot = require("../src/telegram-bot/tbot");
 const getTelegramChatId = require("../src/telegram-bot/get-telegram-chat-id");
 const axios = require("axios");
 
+const getWeekNumber = () => {
+  const week = moment().isoWeek();
+  const day = moment().isoWeekday();
+  const holiday = day === 6 || day === 7;
+  return { weekNumber: (week % 4) + 1, isHoliday: holiday };
+};
+
+async function getLunchWeek(req, res) {
+  return res.json({ status: "OK", data: getWeekNumber() });
+}
+
 async function sendLunchTelegram(req, res) {
   const images = {
-    1: [
+    2: [
       // 2 неделя
       "AgACAgIAAxkBAAIE_2c7e4-0wnoda5jngvuWvMGngaTCAAJC6TEbG0HYSddJfLSNEf8CAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIFAAFnO3uk7dJ-MEfHQcHDogABq93i3-oAAkPpMRsbQdhJNh6V6gXzKPkBAAMCAAN3AAM2BA",
@@ -13,7 +24,7 @@ async function sendLunchTelegram(req, res) {
       "AgACAgIAAxkBAAIFAmc7e8U8Dp9qz1K7-HiBqjP3K0ekAAJF6TEbG0HYSUOsofP4RqLnAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIFA2c7e9Z6MrAsZwxKmN1epV-dEmY3AAJH6TEbG0HYSZG9TQQb2M6FAQADAgADdwADNgQ",
     ],
-    2: [
+    3: [
       // 3 неделя
       "AgACAgIAAxkBAAIE8Gc7edq9EIxXl2qz51DXOLGnrJbjAAIx6TEbG0HYSR3l_xG4Pm4hAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIE8Wc7egKP9QOkbBmlsxGaRgs5OqB6AAIy6TEbG0HYSYmTxnk_WEHqAQADAgADdwADNgQ",
@@ -21,7 +32,7 @@ async function sendLunchTelegram(req, res) {
       "AgACAgIAAxkBAAIE82c7eiMw0qHFku4wqnPTzTRLmKhDAAI36TEbG0HYSe2-Hg8SbmwgAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIE9Gc7elgM1SxFajKrRxH1bnFSLCLuAAI46TEbG0HYSWiulPrBGgK_AQADAgADdwADNgQ",
     ],
-    3: [
+    4: [
       // 4 неделя
       "AgACAgIAAxkBAAIE9Wc7eoUohNY7V1zG7YEI0KqdoAoyAAI66TEbG0HYSYGF65UKefqNAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIE9mc7epsE2467sO1ZLhR94z2k3h0UAAI76TEbG0HYScNLk_Ou8_0qAQADAgADdwADNgQ",
@@ -29,7 +40,7 @@ async function sendLunchTelegram(req, res) {
       "AgACAgIAAxkBAAIE82c7eiMw0qHFku4wqnPTzTRLmKhDAAI36TEbG0HYSe2-Hg8SbmwgAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIE9Gc7elgM1SxFajKrRxH1bnFSLCLuAAI46TEbG0HYSWiulPrBGgK_AQADAgADdwADNgQ",
     ],
-    0: [
+    1: [
       // 1 неделя
       "AgACAgIAAxkBAAIE9Wc7eoUohNY7V1zG7YEI0KqdoAoyAAI66TEbG0HYSYGF65UKefqNAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIE9mc7epsE2467sO1ZLhR94z2k3h0UAAI76TEbG0HYScNLk_Ou8_0qAQADAgADdwADNgQ",
@@ -39,14 +50,13 @@ async function sendLunchTelegram(req, res) {
     ],
   };
 
-  const week = moment().isoWeek();
+  const week = getWeek();
   const day = moment().isoWeekday();
-  const holiday = day === 6 || day === 7;
 
-  if (!holiday) {
+  if (!week.isHoliday) {
     await tbot.sendPhoto(
       getTelegramChatId("channel"),
-      images[week % 4][day - 1]
+      images[week.weekNumber % 4][day - 1]
     );
   }
 
@@ -55,7 +65,7 @@ async function sendLunchTelegram(req, res) {
 
 async function sendLunchVk(req, res, next) {
   const images = {
-    1: [
+    2: [
       // 2 неделя
       "photo-211214337_457244379",
       "photo-211214337_457244378",
@@ -63,7 +73,7 @@ async function sendLunchVk(req, res, next) {
       "photo-211214337_457244382",
       "photo-211214337_457244380",
     ],
-    2: [
+    3: [
       // 3 неделя
       "photo-211214337_457244364",
       "photo-211214337_457244363",
@@ -71,7 +81,7 @@ async function sendLunchVk(req, res, next) {
       "photo-211214337_457244367",
       "photo-211214337_457244365",
     ],
-    3: [
+    4: [
       // 4 неделя
       "photo-211214337_457244369",
       "photo-211214337_457244368",
@@ -79,7 +89,7 @@ async function sendLunchVk(req, res, next) {
       "photo-211214337_457244372",
       "photo-211214337_457244370",
     ],
-    0: [
+    1: [
       // 1 неделя
       "photo-211214337_457244374",
       "photo-211214337_457244373",
@@ -89,19 +99,19 @@ async function sendLunchVk(req, res, next) {
     ],
   };
 
-  const week = moment().isoWeek();
+  const week = getWeek();
   const day = moment().isoWeekday();
-  const holiday = day === 6 || day === 7;
+
   let status = "OK";
 
-  if (!holiday) {
+  if (!week.isHoliday) {
     axios
       .post(
         "https://broadcast.vkforms.ru/api/v2/broadcast?token=api_87768_YKQjQvoekX1ri4HGHKYRG4Wi",
         {
           message: {
             message: "Меню бизнес-ланча на сегодня",
-            attachment: images[week % 4][day - 1],
+            attachment: images[week.weekNumber % 4][day - 1],
           },
           list_ids: "1179243",
           run_now: 1,
@@ -119,4 +129,4 @@ async function sendLunchVk(req, res, next) {
   return res.json({ status });
 }
 
-module.exports = { sendLunchTelegram, sendLunchVk };
+module.exports = { sendLunchTelegram, sendLunchVk, getLunchWeek };
--- routes/lunch.js
+++ routes/lunch.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const lunchControllers = require('../controllers/lunch');
+const lunchControllers = require("../controllers/lunch");
 
 router.get("/menu-for-today", lunchControllers.sendLunchTelegram);
-
 router.get("/menu-for-today-vk", lunchControllers.sendLunchVk);
+router.get("/get-week", lunchControllers.getLunchWeek);
 
 module.exports = router;
