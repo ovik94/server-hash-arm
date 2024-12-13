eeb7652d8a6122dbb9c620cc04aca90e1062b861
Ovik Bagdasaryan <you@example.com>
2024-12-13 20:47:55 +0700

    fix lunch

--- controllers/lunch.js
+++ controllers/lunch.js
@@ -17,7 +17,6 @@ async function getLunchWeek(req, res) {
 async function sendLunchTelegram(req, res) {
   const images = {
     2: [
-      // 2 неделя
       "AgACAgIAAxkBAAIE_2c7e4-0wnoda5jngvuWvMGngaTCAAJC6TEbG0HYSddJfLSNEf8CAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIFAAFnO3uk7dJ-MEfHQcHDogABq93i3-oAAkPpMRsbQdhJNh6V6gXzKPkBAAMCAAN3AAM2BA",
       "AgACAgIAAxkBAAIFAWc7e7d0WKLpqTkSQiarJJv_RID_AAJE6TEbG0HYSd58HpnV0iTRAQADAgADdwADNgQ",
@@ -25,23 +24,20 @@ async function sendLunchTelegram(req, res) {
       "AgACAgIAAxkBAAIFA2c7e9Z6MrAsZwxKmN1epV-dEmY3AAJH6TEbG0HYSZG9TQQb2M6FAQADAgADdwADNgQ",
     ],
     3: [
-      // 3 неделя
+      "AgACAgIAAxkBAAIFP2dcOmpm6pC2BSK9uJ_E45DcSFshAALT6DEbVEDgSoL8ND1ItvY9AQADAgADeQADNgQ",
+      "AgACAgIAAxkBAAIFQGdcOoYdXPtxqO3Nogstecddu2m5AALU6DEbVEDgSisFt8jdcPFqAQADAgADeQADNgQ",
+      "AgACAgIAAxkBAAIFQWdcOpilCdLEvYYYnAPbevRhNbqCAALV6DEbVEDgSqC4ilYQOR1AAQADAgADeQADNgQ",
+      "AgACAgIAAxkBAAIFQmdcOqzvq1Giwt8URFscoHHeiNnGAALW6DEbVEDgSqlM6sYUCx9PAQADAgADeQADNgQ",
+      "AgACAgIAAxkBAAIFQ2dcOsHguS5AZoeu8YzEaMMvlr9YAALb6DEbVEDgSpIsuKDFCqH5AQADAgADeQADNgQ",
+    ],
+    4: [
       "AgACAgIAAxkBAAIE8Gc7edq9EIxXl2qz51DXOLGnrJbjAAIx6TEbG0HYSR3l_xG4Pm4hAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIE8Wc7egKP9QOkbBmlsxGaRgs5OqB6AAIy6TEbG0HYSYmTxnk_WEHqAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIE8mc7ehKzzXSgbBscbA6z-c3-Y0eeAAI26TEbG0HYSd4_Rl0fxhOGAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIE82c7eiMw0qHFku4wqnPTzTRLmKhDAAI36TEbG0HYSe2-Hg8SbmwgAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIE9Gc7elgM1SxFajKrRxH1bnFSLCLuAAI46TEbG0HYSWiulPrBGgK_AQADAgADdwADNgQ",
     ],
-    4: [
-      // 4 неделя
-      "AgACAgIAAxkBAAIE9Wc7eoUohNY7V1zG7YEI0KqdoAoyAAI66TEbG0HYSYGF65UKefqNAQADAgADdwADNgQ",
-      "AgACAgIAAxkBAAIE9mc7epsE2467sO1ZLhR94z2k3h0UAAI76TEbG0HYScNLk_Ou8_0qAQADAgADdwADNgQ",
-      "AgACAgIAAxkBAAIE8mc7ehKzzXSgbBscbA6z-c3-Y0eeAAI26TEbG0HYSd4_Rl0fxhOGAQADAgADdwADNgQ",
-      "AgACAgIAAxkBAAIE82c7eiMw0qHFku4wqnPTzTRLmKhDAAI36TEbG0HYSe2-Hg8SbmwgAQADAgADdwADNgQ",
-      "AgACAgIAAxkBAAIE9Gc7elgM1SxFajKrRxH1bnFSLCLuAAI46TEbG0HYSWiulPrBGgK_AQADAgADdwADNgQ",
-    ],
     1: [
-      // 1 неделя
       "AgACAgIAAxkBAAIE9Wc7eoUohNY7V1zG7YEI0KqdoAoyAAI66TEbG0HYSYGF65UKefqNAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIE9mc7epsE2467sO1ZLhR94z2k3h0UAAI76TEbG0HYScNLk_Ou8_0qAQADAgADdwADNgQ",
       "AgACAgIAAxkBAAIE_Gc7e2MZLCk3fRaHVJg0i2buG9PTAAI_6TEbG0HYSdwzRy0-Pl44AQADAgADdwADNgQ",
@@ -66,7 +62,6 @@ async function sendLunchTelegram(req, res) {
 async function sendLunchVk(req, res, next) {
   const images = {
     2: [
-      // 2 неделя
       "photo-211214337_457244379",
       "photo-211214337_457244378",
       "photo-211214337_457244381",
@@ -74,7 +69,6 @@ async function sendLunchVk(req, res, next) {
       "photo-211214337_457244380",
     ],
     3: [
-      // 3 неделя
       "photo-211214337_457244364",
       "photo-211214337_457244363",
       "photo-211214337_457244366",
@@ -82,7 +76,6 @@ async function sendLunchVk(req, res, next) {
       "photo-211214337_457244365",
     ],
     4: [
-      // 4 неделя
       "photo-211214337_457244369",
       "photo-211214337_457244368",
       "photo-211214337_457244371",
@@ -90,7 +83,6 @@ async function sendLunchVk(req, res, next) {
       "photo-211214337_457244370",
     ],
     1: [
-      // 1 неделя
       "photo-211214337_457244374",
       "photo-211214337_457244373",
       "photo-211214337_457244376",
